5c091aa357884517f8ad5a8f7ac9d14108747c11
pavelixo <trashxindoragon87@gmail.com>
2025-03-06 19:35:01 -0300

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-pip3 install -r requirements.txt
-python3 manage.py collectstatic --noinput
+
 npm install
 npm run build
+
+pip3 install -r requirements.txt
+python3 manage.py collectstatic --noinput
