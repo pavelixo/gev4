c1a14357774bdf0e0d63bd682c7eb51fd3ccb21f
pavelixo <trashxindoragon87@gmail.com>
2025-03-06 19:02:05 -0300

    remove log

--- core/urls.py
+++ core/urls.py
@@ -4,19 +4,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.http import HttpResponse
-
-def debug(request):
-    return HttpResponse(
-        f'DEBUG: {settings.DEBUG}\n' +
-        f'SECRET_KEY: {settings.SECRET_KEY}\n'
-        f'DEFAULT: {settings.DATABASES["default"]}\n', 
-        content_type='text/plain'
-    )
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('debug/', debug)
 ]
 
 if settings.DEBUG:
