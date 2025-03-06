3a7b75da4d1cdacc339000206cd93d253d91940e
pavelixo <trashxindoragon87@gmail.com>
2025-03-06 18:45:25 -0300

    ...

--- core/settings.py
+++ core/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load(BASE_DIR / ".env", override=True)
-load(BASE_DIR / ".env.example", override=False)
+# load(BASE_DIR / ".env.example", override=False)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env(
